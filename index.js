846bb84cb60428592ce5261f7a65e1ddb073ab3d
James Aules <james_aules@icloud.com>
2023-06-12 13:51:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Modules and Globals
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const path = require("path")
