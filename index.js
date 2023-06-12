b91c64abeac0629f6339102dade138e37b6f53ff
James Aules <james_aules@icloud.com>
2023-06-12 19:02:11 -0400

    packages config & mongoDB

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const path = require("path")
+
+// Express Settings
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`);
+  });
+  
