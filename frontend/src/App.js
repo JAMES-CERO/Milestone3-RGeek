725807a1b69e78ffc9b8acda920c642447ce8645
James Aules <james_aules@icloud.com>
2023-06-14 01:02:14 -0400

    state RR, routes RRD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom'
+import homePage from './views/homePage';
+import navBar from './views/navbar';
+import loginPage from './views/loginPage';
+import profilePage from './views/profilePage';
+
 function App() {
-  return  <div className="app">  </div>
+  return (
+      <div className="app"> 
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<loginPage />} />
+                  <Route path='/home' element={<homePage />} />
+                  <Route path='/profile/:userId' element={< profilePage />} />
+              </Routes>
+          </BrowserRouter>
+      </div>
+  )
 }
 
 export default App;
