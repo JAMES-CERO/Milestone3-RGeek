aa1ebfb5bfd72caeb17071cab787772118b4e4ed
James Aules <james_aules@icloud.com>
2023-06-14 19:22:41 -0400

    redux toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import homePage from './views/homePage';
 import navBar from './views/navbar';
 import loginPage from './views/loginPage';
 import profilePage from './views/profilePage';
+import authReducer from "./state";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
