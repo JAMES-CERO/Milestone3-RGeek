9eedd95265fc831cadf7acf527d478f3b0fbad01
James Aules <james_aules@icloud.com>
2023-06-17 14:15:00 -0400

    modules, packages

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,46 @@
 // Modules and Globals
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-const path = require("path")
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from 'path';
+import multer from "multer";
+import morgan from "morgan";
+import helmet from "helmet";
+import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js'
+dotenv.config();
 
 // Express Settings
+const app = express();
+
+//modules - package.json
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb",extended: true }));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+//FILE STORAGE 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  fileName: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+const upload = multer({storage});
+
+// Routes & files - middleware
+app.post("/auth/register", upload.single('picture'), register)
+
 
 
 // Listen for Connections
