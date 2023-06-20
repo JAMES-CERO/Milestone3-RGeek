637252bfc30093c012b69334daba26b9a4b09e0e
James Aules <james_aules@icloud.com>
2023-06-19 20:19:53 -0400

    hotfix timeStamps

--- backend/models/User.js
+++ backend/models/User.js
@@ -34,7 +34,9 @@ const UserSchema = new mongoose.Schema({
     },
     location: String,
     occupation: String
-})
+},
+{ timestamps: true}
+);
 
 const User = mongoose.model('User', UserSchema)
 
