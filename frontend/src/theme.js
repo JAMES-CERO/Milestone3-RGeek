47bbe781e703089e0a06a52322100d9c47d1f0f2
James Aules <james_aules@icloud.com>
2023-06-24 13:05:41 -0400

    theme.js

---
+++ frontend/src/theme.js
@@ -0,0 +1,12 @@
+export const colorTokens = {
+    dark: {
+        background: "#000000",
+        header: "#FF0000",
+        icon: "#F55968",
+    },
+    light: {
+        background: "#FFFFFF",
+        header: "#FF0000",
+        icon: "#F55968",
+    },
+};
--- frontend/src/views/loginPage/index.jsx
+++ frontend/src/views/loginPage/index.jsx
@@ -1,21 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { 
+    Box,
+    Typography, 
+    useTheme, 
+    useMediaQuery 
+} from "@mui/material";
 
 const LoginPage = () => {
+    const theme = useTheme();
+    const noMobileScreen = useMediaQuery("(min-width: 1000px)");
     return (
         <Box>
-            <Box width='100%' textAlign='center'>
-            <Typography
-                fontWeight='bold'
-                fontSize= '32px'
-                color='red'
-                backgroundColor='black'
+            <Box width='100%'  textAlign='center' p='1 rem 6%'>
+            <Typography fontWeight='bold' fontSize= '32px' color='red' backgroundColor='black'
             >
                 RGeeks
             </Typography>
             </Box>
 
-            <Box width="50%" backgroundColor='green'>
-                <Typography fontWeight="500" variant="h5">
+            <Box width={ noMobileScreen ? "50%" : "93%"} p="2rem" m="2rem auto" borderRadius="1.5rem" >
+                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
                     Welcome to Red Geeks!
                 </Typography>
             </Box>
